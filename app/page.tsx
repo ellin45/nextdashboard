cc2401fbb97d10b6aaf416af7d43cc144b577d9f
정채림 <chaelimj45@gmail.com>
2024-09-03 21:53:20 +0900

    "feat: summary on the dashboard"

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Summary from "@/components/dashboard/summary";
+
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-4 grid gap-5">
+      <Summary/>
+   
+    </div>
+  )
 }
